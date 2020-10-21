7c91ead1589c0e6461aa601df3a75c57e2be0884
vssh77 <vssh77@gmail.com>
2020-10-21 21:02:11 +0900

    Update Reservation.java

--- src/main/java/refuel/Reservation.java
+++ src/main/java/refuel/Reservation.java
@@ -25,7 +25,8 @@ public class Reservation {
     public void onPrePersist(){
         
         try {
-            Thread.currentThread().sleep((long) (800 + Math.random() * 620));
+            //Thread.currentThread().sleep((long) (800 + Math.random() * 620));
+            Thread.currentThread().sleep((long) (3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
