3417fd6243ca23d8fae629ede108c4215aa7c7dd
SeokHeeJeong <vssh77@gmail.com>
2020-10-21 18:00:36 +0900

    부하 발생기 적용

--- src/main/java/refuel/Reservation.java
+++ src/main/java/refuel/Reservation.java
@@ -21,6 +21,15 @@ public class Reservation {
     private Long price;
     private Long refuelId;
 
+    @PrePersist
+    public void onPrePersist(){
+        /*
+        try {
+            Thread.currentThread().sleep((long) (800 + Math.random() * 220));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+    }
 
     @PostPersist
     public void onPostPersist(){
